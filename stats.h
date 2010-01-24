d134aa492760685cb740da120d7509f198ee27cb
Kurt Stephens <ks.github@kurtstephens.com>
2010-01-24 05:40:20 +0000

    Document tm_time_stat.

--- stats.h
+++ stats.h
@@ -10,16 +10,17 @@
  * Timing statstics.
  */
 typedef struct tm_time_stat {
-  const char *name;
-  double 
-    td, /* Last time. */
-    ts, /* Total time. */
-    tw, /* Worst time. */
-    ta, /* Avg time. */
-    t0, t1, 
-    t01, t11;
-  short tw_changed;
-  unsigned int count;
+  const char *name;   /*!< The name for this statistic. */
+  double td;          /*!< Last time in seconds. */
+  double ts;          /*!< Total time in seconds. */
+  double tw;          /*!< Worst time in seconds. */
+  double ta;          /*!< Average time in seconds. */
+  double t0;          /*!< Absolute time at tm_time_stat_begin(). */
+  double t1;          /*!< Absolute time at tm_time_stat_end(). */ 
+  double t01;         /*!< Aux time at tm_time_stat_begin(). */
+  double t11;         /*!< Aux time at tm_time_stat_end(). */
+  short tw_changed;   /*!< Time worst changed? */
+  unsigned int count; /*!< Total calls to tm_time_stat_end(). */
 } tm_time_stat;
 
 void tm_time_stat_begin(tm_time_stat *ts);
